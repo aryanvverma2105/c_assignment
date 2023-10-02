d6b56e90f9e75a9175ab7ecf5f646f70d3a1de87
aryanvverma2105 <146726824+aryanvverma2105@users.noreply.github.com>
2023-10-02 17:09:42 +0530

    Add files via upload

---
+++ asmd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    printf ("Enter a");
+    scanf ("%d", &a);
+    printf ("enter b");
+    scanf ("%d", &b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf ("\n Addition of two number=%d", c);
+printf ("\n subtraction of two number=%d", d);
+printf ("\n multiplication of two number=%d", e);
+printf ("\n division of two number=%d", f);
+}
